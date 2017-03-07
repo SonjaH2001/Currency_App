72ebb697683e0a6e620a1991b166bf3631c6f285
SH <email@email.com>
2017-03-07 15:00:37 -0600

    Added exchange API

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
-var exchangeRates = { 'EUR': 0.94, 'JPY' : 112.86, 'USA' : 1.00 };
+var currency = require('../helpers/currencyRates')//goes back a directory and finds the file
+// var exchangeRates = { 'EUR': 0.94, 'JPY' : 112.86, 'USA' : 1.00 };
 
 /* handle GET request for homepage*/
 router.get('/', function(req, res){
@@ -14,16 +14,21 @@ router.get('/', function(req, res){
 //     res.render('index');
 
 router.get('/convert', function(req, res) {
-    var amount = req.query.baseCurrency_amount;
-    var convertTo = req.query.to_currency;
-    var convertFrom = req.query.from_currency;
-    //res.send ('to do: convert..)
-    var baseToDollarsRate = exchangeRates[convertFrom];
-    var amountOfDollars = amount/baseToDollarsRate;
-    var dollarsToTargetRate = exchangeRates[convertTo];
-    result = dollarsToTargetRate * amountOfDollars;
+    currency(function (exchangeRates, error) {
+            var amount = req.query.baseCurrency_amount;
+            var convertTo = req.query.to_currency;
+            var convertFrom = req.query.from_currency;
+            //res.send ('to do: convert..)
+            var baseToDollarsRate = exchangeRates[convertFrom];
+            var amountOfDollars = amount/baseToDollarsRate;
+            var dollarsToTargetRate = exchangeRates[convertTo];
+            result = dollarsToTargetRate * amountOfDollars;
 
-    res.render('results', { inputAmount : amount, result : result,fromCurrency : convertFrom , toCurrency: convertTo})
+            res.render('results', { inputAmount : amount, result : result,fromCurrency : convertFrom , toCurrency: convertTo})
+    }
+
+    )
+
 });
 
 module.exports = router;
